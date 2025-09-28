6c2f07dcd8ec5050731b0be252e3492c62afc82c
Sxuoi <stafgaming5@gmail.com>
2025-09-28 21:30:32 +0700

    modified:   src/App.jsx 	new file:   src/WelcomePage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './WelcomePage.jsx'
+
+function MyButton() {
+    return (
+        <button>
+            I'm a button!
+            </button>
+    )
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +38,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <MyButton/>
     </>
   )
 }
---
+++ src/WelcomePage.jsx
@@ -0,0 +1,11 @@
+function WelcomePage() {
+    return (
+        <div className="welcome-container">
+            <h1>Selamat Datang di Wardrobe Manager</h1>
+            <p>Aplikasi modern untuk mengelola koleksi pakaian kamu!</p>
+            <button className="start-button">
+                Mulai Mengatur Wardrobe
+            </button>
+        </div>
+    );
+}
